8880ff2978352d072a55b04cd705e39c43a0dbe9
Dilum Aluthge <dilum@aluthge.com>
2021-03-22 18:45:47 -0400

    Move the REPL functionality from GitCommand.jl to GitRepl.jl (#5)

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://JuliaVersionControl.github.io/GitRepl.jl/dev)
 [![Build Status](https://github.com/JuliaVersionControl/GitRepl.jl/workflows/CI/badge.svg)](https://github.com/JuliaVersionControl/GitRepl.jl/actions)
 [![Coverage](https://codecov.io/gh/JuliaVersionControl/GitRepl.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/JuliaVersionControl/GitRepl.jl)
+
+## Git REPL mode
+
+```julia
+julia> using GitRepl
+
+julia> gitrepl() # you only need to run this once per Julia session
+
+# Press , to enter the Git REPL mode
+
+git> clone https://github.com/JuliaRegistries/General
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using GitRepl
 using Test
 
+function withtempdir(f::Function)
+    original_directory = pwd()
+    mktempdir() do tmp_dir
+        return f(tmp_dir)
+    end
+    cd(original_directory)
+    return nothing
+end
+
 @testset "GitRepl.jl" begin
+    withtempdir() do tmp_dir
+        @test !isdir("GitRepl.jl")
+        @test !isfile(joinpath("GitRepl.jl", "Project.toml"))
+        expr = GitRepl._gitrepl_parser("clone https://github.com/JuliaVersionControl/GitRepl.jl")
+        @test expr isa Expr
+        @test !isdir("GitRepl.jl")
+        @test !isfile(joinpath("GitRepl.jl", "Project.toml"))
+        @eval $(expr)
+        @test isdir("GitRepl.jl")
+        @test isfile(joinpath("GitRepl.jl", "Project.toml"))
+    end
 end
