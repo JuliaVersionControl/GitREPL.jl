819b871c9935a109e8725ce77a3330bc9c246c65
Dilum Aluthge <dilum@aluthge.com>
2021-03-22 18:21:06 -0400

    Move the REPL functionality from GitCommand.jl to GitRepl.jl

--- src/GitRepl.jl
+++ src/GitRepl.jl
@@ -3,4 +3,32 @@ module GitRepl
 import GitCommand
 import ReplMaker
 
+export gitrepl
+
+const GIT_REPL_MODE_NAME = "GitRepl.jl Git REPL mode"
+const GIT_REPL_MODE_PROMPT_TEXT = "git> "
+const GIT_REPL_MODE_START_KEY = ','
+
+function _gitrepl_parser(repl_input::AbstractString)
+    return quote
+        repl_input = $(Expr(:quote, repl_input))
+        run(`$(GitRepl.GitCommand.git()) $(split(repl_input))`)
+        return nothing
+    end
+end
+
+function gitrepl(; mode_name = GIT_REPL_MODE_NAME,
+                   prompt_text = GIT_REPL_MODE_PROMPT_TEXT,
+                   start_key = GIT_REPL_MODE_START_KEY,
+                   kwargs...)
+    ReplMaker.initrepl(
+        _gitrepl_parser;
+        mode_name = mode_name,
+        prompt_text = prompt_text,
+        start_key = start_key,
+        kwargs...,
+    )
+    return nothing
+end
+
 end # module
