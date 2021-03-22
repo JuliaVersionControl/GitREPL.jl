ee3b93be96f910e792e8a6b38b8ef8b4ba71aeed
Documenter.jl <documenter@juliadocs.github.io>
2021-03-22 22:05:33 +0000

    build based on 6ba5a2f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GitRepl","category":"page"},{"location":"#GitRepl","page":"Home","title":"GitRepl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GitRepl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GitRepl]","category":"page"}]
+}
